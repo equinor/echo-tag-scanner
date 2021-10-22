f660e184308ce00b74b0343041b9f02a00538354
Erlend Andreas Hall <ehall@equinor.com>
2021-10-22 14:25:23 +0200

    made some preperations for future scanning tasks

---
+++ src/services/scanning/Scanner.ts
@@ -0,0 +1,39 @@
+import { getNotificationDispatcher } from '@utils';
+
+const mockedTagNumbers = ['63JA001', 'tagnumber123', ''];
+
+async function doScanning(): Promise<string | undefined> {
+  const response = await apiFetch();
+  const inspectionResult = inspectApiResult(response);
+
+  if (inspectionResult) {
+    getNotificationDispatcher('Found ' + response)();
+  } else {
+    return undefined;
+  }
+}
+
+function inspectApiResult(result: string): boolean {
+  // Inspect the tag number here
+  if (result) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function apiFetch(): Promise<string> {
+  // Do API fetch here, waiting and stuffs.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(mockedTagNumbers[0]);
+    }, 1000);
+  });
+}
+
+function apiRejection(reason: unknown) {
+  // Handle errors here, 404, 502 and stuffs.
+  // Use notification or let the issue go unhandled to make ErrorBoundary do the work.
+}
+
+export { doScanning };

7ca4391cc0e03f0973b0b02dd1e1596c215a3620
Erlend Andreas Hall <ehall@equinor.com>
2022-04-07 10:46:56 +0200

    added some request timer logging

--- src/services/api/ocr/ocr.ts
+++ src/services/api/ocr/ocr.ts
@@ -6,10 +6,24 @@ import { MadOCRFunctionalLocations } from '@types';
 
 export async function getFunctionalLocations(image: Blob): Promise<MadOCRFunctionalLocations> {
   try {
+    const beforeRequestTimestamp = new Date();
     const { url, body, init } = getFunctionalLocationsResources(image);
-    return (await baseApiClient.postAsync<MadOCRFunctionalLocations>(url, body, init)).data;
+    const result = await baseApiClient.postAsync<MadOCRFunctionalLocations>(
+      url,
+      body,
+      init
+    );
+    const afterRequestTimestamp = new Date();
+    reportTimeTakenForRequest(beforeRequestTimestamp, afterRequestTimestamp);
+    return result.data;
   } catch (error) {
     throw handleError(ErrorRegistry.ocrError, error as Error);
   }
 }
 export const ocrApi = { getFunctionalLocations };
+
+function reportTimeTakenForRequest(startDate: Date, endDate: Date) {
+  const result = endDate.getSeconds() - startDate.getSeconds();
+  console.group('Request timer');
+  console.info(`Request took -> ${result} seconds`);
+}

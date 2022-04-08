81744b26f7f687d95de99f4b81199668a355b73d
Erlend Andreas Hall <ehall@equinor.com>
2022-04-08 14:11:51 +0200

    improved tag filtering and presentation

--- src/components/searchresults/SearchResults.tsx
+++ src/components/searchresults/SearchResults.tsx
@@ -1,48 +1,85 @@
-import React from "react";
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import { Button } from '@equinor/eds-core-react';
+import EchoUtils from '@equinor/echo-utils';
 import { TagContextMenu, TagIcon, getIcon } from '@equinor/echo-components';
 import { getLegendStatusColor } from '@equinor/echo-framework';
-import { TagSummaryDto } from '@equinor/echo-search';
+import { ExtractedFunctionalLocation } from '@types';
+import { Search, TagSummaryDto } from '@equinor/echo-search';
+import { getInstCode } from '@utils';
 
 interface SearchResultsProps {
-  tags: TagSummaryDto[];
+  functionalLocations: ExtractedFunctionalLocation[];
   onTagSearch: (tagNumber: string) => void;
   onClose: () => void;
 }
 
 const SearchResults = (props: SearchResultsProps): JSX.Element => {
-  function onClick(tagNumber?: string) {
-    if (tagNumber) {
-      props.onTagSearch(tagNumber);
+  const [tagSummary, setTagSummary] = useState<TagSummaryDto[]>(undefined);
+
+  /**
+   * Keeps and updates a list of tag summaries for user presentation.
+   */
+  EchoUtils.Hooks.useEffectAsync(async () => {
+    if (props.functionalLocations.length > 0) {
+      const result = await Search.Tags.getAllAsync(
+        props.functionalLocations.map((l) => l.tagNumber)
+      );
+      if (result.isSuccess) {
+        logTagSummaries(result.values);
+        setTagSummary(result.values);
+      } else {
+        setTagSummary(undefined);
+      }
     }
+  }, [props.functionalLocations]);
+
+  function onClick(tagNumber: string) {
+    props.onTagSearch(tagNumber);
   }
 
-  return (
-    <InvisibleWrapper>
-      {props.tags.map((tag, index) => (
-        //@ts-ignore
-        // Ignoring a non-optional (setExpanded) prop as this one should be optional.
-        <SearchResult
-          key={index}
-          expanded
-          description={tag.description}
-          openTagInformation={() => onClick(tag.tagNo)}
-          tagNo={tag.tagNo}
-          selected={false}
-        >
-          <TagIcon
-            icon={getIcon(tag.tagCategoryDescription)}
-            legendColor={getLegendStatusColor(tag.tagStatus)}
-          />
-        </SearchResult>
-      ))}
-      <ScanAgainButton variant="contained" onClick={props.onClose}>
-        Scan again
-      </ScanAgainButton>
-    </InvisibleWrapper>
-  );
+  function logTagSummaries(tagSummaries: TagSummaryDto[]) {
+    console.group('This tag data is used for presentation');
+    if (tagSummaries.length > 0) {
+      tagSummaries.forEach((tag) => console.table(tag));
+    }
+
+    console.groupEnd();
+  }
+
+  function createSearchResult(tag: TagSummaryDto, index: number) {
+    return (
+      //@ts-ignore
+      // Ignoring a non-optional (setExpanded) prop as there is
+      // no need to handle expanded states.
+      <SearchResult
+        key={index}
+        expanded
+        openTagInformation={() => onClick(tag.tagNo)}
+        tagNo={tag.tagNo}
+        selected={false}
+      >
+        <TagIcon
+          icon={getIcon(tag.tagCategoryDescription)}
+          legendColor={getLegendStatusColor(tag.tagStatus)}
+        />
+      </SearchResult>
+    );
+  }
+
+  if (Array.isArray(tagSummary) && tagSummary.length > 0) {
+    return (
+      <InvisibleWrapper>
+        {tagSummary.map(createSearchResult)}
+        <ScanAgainButton variant="contained" onClick={props.onClose}>
+          Scan again
+        </ScanAgainButton>
+      </InvisibleWrapper>
+    );
+  } else {
+    return null;
+  }
 };
 
 const ScanAgainButton = styled(Button)``;
--- src/utils/tagFiltering.ts
+++ src/utils/tagFiltering.ts
@@ -1,10 +1,6 @@
 import { ExtractedFunctionalLocation, MadOCRFunctionalLocations } from '@types';
-import { ResultValue, Search, TagSummaryDto } from '@equinor/echo-search';
+import { Search, TagSummaryDto } from '@equinor/echo-search';
 import { extractFunctionalLocation, getInstCode } from '@utils';
-import { getSelectedPlant } from '@equinor/echo-core';
-import {
-  getNotificationDispatcher as dispatchNotification,
-} from '@utils';
 
 /**
  * Returns a promise to return a TagSummary, or undefined if no tags were found.
@@ -12,21 +8,29 @@ import {
 function createTagSearch(
   location: ExtractedFunctionalLocation,
   instCode?: string
-): Promise<TagSummaryDto | undefined> {
-  return new Promise(function getTag(resolve, reject) {
-    Search.Tags.searchAsync(location.tagNumber, 1, instCode)
-      .then(function handleTagValidationResponse(tagSummary) {
-        console.group('Looking up tag number: ', location.tagNumber);
-        console.info('Lookup result: ', tagSummary.values);
-        console.groupEnd();
-        if (tagSummary.values.length === 1) {
-          resolve(tagSummary.values[0]);
+): Promise<ExtractedFunctionalLocation | undefined> {
+  return new Promise((resolve, reject) => {
+    Search.Tags.isTagAsync(location.tagNumber, instCode)
+      .then(function handleTagValidationResponse(validation) {
+        if (Boolean(validation.value)) {
+          console.info(
+            `${location.tagNumber} is valid: `,
+            Boolean(validation.value)
+          );
+        } else {
+          console.warn(
+            `${location.tagNumber} is valid: `,
+            Boolean(validation.value)
+          );
+        }
+
+        if (validation.value) {
+          resolve(location);
         } else {
           resolve(undefined);
         }
       })
       .catch(function handleTagValidationError(reason) {
-        console.log('%c⧭', 'color: #994d75', reason);
         // TODO Possible errorboundary target
         reject(reason);
       });
@@ -38,30 +42,24 @@ function getFunctionalLocations(locations: MadOCRFunctionalLocations) {
 }
 
 /**
- * Filters away falsey tag results for prior tag searches.
- */
-function filterInvalidTags(res: Array<TagSummaryDto | undefined>) {
-  return res.filter((r) => Boolean(r));
-}
-
-/**
  * The user might have scanned images containing other things than tag-numbers.
  * This function will filter them out.
  */
 export function tagSearch(
   locations: MadOCRFunctionalLocations,
   afterSearchCallback: () => void
-): Promise<TagSummaryDto[]> {
+): Promise<ExtractedFunctionalLocation[]> {
   return new Promise((resolve) => {
     const functionalLocations = getFunctionalLocations(locations);
-    console.log('%c⧭', 'color: #00258c', functionalLocations);
     const tagSearches = functionalLocations.map((funcLocation) =>
       createTagSearch(funcLocation, getInstCode() ?? 'TROA')
     );
 
     Promise.all([...tagSearches]).then((results) => {
       afterSearchCallback();
-      resolve(filterInvalidTags(results));
+
+      // Resolve a list of validated tags and filter away undefines.
+      resolve(results.filter((r) => Boolean(r)));
     });
   });
 }

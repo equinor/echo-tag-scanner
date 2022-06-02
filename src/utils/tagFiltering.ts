dd499f39b98d138ec0a111a4b85c579b369d05d8
Erlend Andreas Hall <ehall@equinor.com>
2022-06-02 10:30:35 +0200

    improved tag validation

--- src/utils/tagFiltering.ts
+++ src/utils/tagFiltering.ts
@@ -1,6 +1,45 @@
-import { ExtractedFunctionalLocation, PossibleFunctionalLocations } from '@types';
-import { Search } from '@equinor/echo-search';
+import {
+  ExtractedFunctionalLocation,
+  PossibleFunctionalLocations
+} from '@types';
+import { Search, TagSummaryDto, ResultValue } from '@equinor/echo-search';
 import { extractFunctionalLocation, getInstCode } from '@utils';
+
+function hasContent(data: unknown) {
+  if (typeof data === 'string' || typeof data === 'number') {
+    return data != null;
+  } else if (Array.isArray(data)) {
+    return data.length > 0;
+  } else if (typeof data === 'object') {
+    return Reflect.ownKeys(data).length > 0;
+  }
+}
+
+/**
+ * Accepts a possible tag number as string value and runs it through Echo Search for validation.
+ */
+async function findClosestTag(possibleTagNumber: string) {
+  const result = await Search.Tags.closestTagAsync(possibleTagNumber);
+  if (result.isSuccess) {
+    console.group('Running validation for ', possibleTagNumber);
+    console.info(possibleTagNumber + ' corrected to ' + result.value);
+    console.groupEnd();
+    return result.value;
+  }
+}
+
+/**
+ * Accepts a validated tag and fetches its tag summary locally.
+ */
+async function getTagSummary(validationResult: string) {
+  const result = await Search.Tags.getAsync(validationResult);
+  if (result.isSuccess && hasContent(result.value)) return result.value;
+}
+
+function tagSummary(tagSummary: TagSummaryDto): TagSummaryDto {
+  // TODO: Validate tag summary
+  return tagSummary;
+}
 
 /**
  * Returns a promise to validate a string as a tag number.
@@ -8,26 +47,12 @@ import { extractFunctionalLocation, getInstCode } from '@utils';
 function createTagValidator(
   location: ExtractedFunctionalLocation,
   instCode?: string
-): Promise<ExtractedFunctionalLocation | undefined> {
+): Promise<TagSummaryDto> {
   return new Promise((resolve, reject) => {
-    Search.Tags.getAsync(location.tagNumber)
-      .then(function handleTagValidationResponse(validation) {
-        console.log('%c⧭', 'color: #731d1d', validation);
-
-        if (validation.isNotFound){
-          console.warn(
-            `${location.tagNumber} is not valid.`,
-            );
-            resolve(location);
-        } else {          
-          console.info(
-            `${location.tagNumber} is valid.`,
-          );
-          resolve(undefined);
-        }
-      })
+    findClosestTag(location.tagNumber)
+      .then(getTagSummary)
+      .then((summary) => resolve(tagSummary(summary)))
       .catch(function handleTagValidationError(reason) {
-        // TODO Possible errorboundary target
         reject(reason);
       });
   });
@@ -40,19 +65,23 @@ function createTagValidator(
 export async function runTagValidation(
   locations: PossibleFunctionalLocations,
   afterSearchCallback: () => void
-): Promise<ExtractedFunctionalLocation[]> {
-
+): Promise<TagSummaryDto[]> {
   // Split the possible tag number into functional locations.
-  const functionalLocations = locations.results.map((l) => extractFunctionalLocation(l));
-    const tagValidationTasks = functionalLocations.map((funcLocation) =>
-      createTagValidator(funcLocation, getInstCode())
-    );
-  const tagValidationResults = await Promise.allSettled([...tagValidationTasks]);
+  const functionalLocations = locations.results.map((l) =>
+    extractFunctionalLocation(l)
+  );
+  const tagValidationTasks = functionalLocations.map((funcLocation) =>
+    createTagValidator(funcLocation, getInstCode())
+  );
+  const tagValidationResults = await Promise.allSettled([
+    ...tagValidationTasks
+  ]);
+  console.log('%c⧭', 'color: #807160', tagValidationResults);
   afterSearchCallback();
-  
-  return tagValidationResults.map((result) => {
-    if (result.status === "fulfilled") return result.value;
-  }).filter(result => Boolean(result));
+
+  return tagValidationResults
+    .map((result) => {
+      if (result.status === 'fulfilled') return result.value;
+    })
+    .filter((result) => Boolean(result));
 }
-
-

4222597039655b7d88ea9b349e7f4c5822f82633
Erlend Andreas Hall <ehall@equinor.com>
2022-06-02 13:35:58 +0200

    switched to older computer vision model

--- src/services/api/resources/resources.ts
+++ src/services/api/resources/resources.ts
@@ -7,7 +7,9 @@ export function getFunctionalLocationsResources(image: Blob): {
   body: Blob;
   init: RequestInit;
 } {
-  const boundary = `------------------------------${Math.random().toString(36).substring(2)}`;
+  const boundary = `------------------------------${Math.random()
+    .toString(36)
+    .substring(2)}`;
 
   // request url
   const url = combineUrls(BaseResources.baseApiUrl, 'ocr', 'get-tags');
@@ -23,7 +25,6 @@ export function getFunctionalLocationsResources(image: Blob): {
     return new Blob([appendingMeta, imageBlob, prependingMeta]);
   }
 
-
   return {
     url,
     body: createBlob(image, boundary),
@@ -36,65 +37,61 @@ export function getFunctionalLocationsResources(image: Blob): {
 type EndpointInfo = {
   url: string;
   apiKey: Array<string>;
-}
+};
 // TODO: Move to env vars.
 const computerVisionOcrEndpointDev: EndpointInfo = {
-  url: "https://echocamera-tag-scanner-dev.cognitiveservices.azure.com/vision/v3.2/ocr",
-  apiKey: ["47a223e8dae44f93ab479f0b49b7005d", "5442094e81954068bd0b1a6df5a1c0ad"]
-}
+  url: 'https://echocamera-tag-scanner-dev.cognitiveservices.azure.com/vision/v3.2/ocr',
+  apiKey: [
+    '47a223e8dae44f93ab479f0b49b7005d',
+    '5442094e81954068bd0b1a6df5a1c0ad'
+  ]
+};
 
 type ComputerVisionOcrParams = {
-  language: string | "unk";
+  language: string | 'unk';
   detectOrientation?: boolean;
-  modelVersion: string | "latest";
-}
+  modelVersion: string | 'latest';
+};
 
-export function getComputerVisionOcrResources(image: Blob): [
-  url: string,
-  body: Blob,
-  requestInit: RequestInit,
- ] {
+export function getComputerVisionOcrResources(
+  image: Blob
+): [url: string, body: Blob, requestInit: RequestInit] {
   const body = image;
 
   let url = computerVisionOcrEndpointDev.url;
   const params: ComputerVisionOcrParams = {
-    language: "unk",
+    language: 'unk',
     detectOrientation: false,
-    modelVersion: "latest"
-  }
+    modelVersion: '2022-04-30'
+  };
   url = addParams(url, params);
-  
+
   const init: RequestInit = {
     headers: {
-      "Content-Type": "application/octet-stream",
-      "Ocp-Apim-Subscription-Key": computerVisionOcrEndpointDev.apiKey[0]
+      'Content-Type': 'application/octet-stream',
+      'Ocp-Apim-Subscription-Key': computerVisionOcrEndpointDev.apiKey[0]
     }
   };
 
-  return [
-    url,
-    body,
-    init
-  ]
+  return [url, body, init];
 }
-
-
 
 export type Params = { [key: string]: any };
 export function addParams(url: string, params: Params): string {
-    const queryString = Object.keys(params)
-      .filter((key) => params[key])
-      .reduce(
-        (query, key) => query + `${query ? '&' : ''}${key}=${encodeURIComponent(params[key])}`,
-        ''
-      );
-  
-    if (queryString) {
-      return url + '?' + queryString;
-    }
-  
-    return url;
+  const queryString = Object.keys(params)
+    .filter((key) => params[key])
+    .reduce(
+      (query, key) =>
+        query + `${query ? '&' : ''}${key}=${encodeURIComponent(params[key])}`,
+      ''
+    );
+
+  if (queryString) {
+    return url + '?' + queryString;
   }
+
+  return url;
+}
 
 export default {
   getFunctionalLocationsResources

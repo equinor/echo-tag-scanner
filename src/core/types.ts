f8d016aa59002f86b9f9f95b7c1e7f79bc555bfd
Erlend Andreas Hall <ehall@equinor.com>
2022-07-27 08:51:07 +0200

    deleted misplaced file

--- src/core/types.ts
+++
@@ -1,8 +0,0 @@
-export interface CameraProps {
-  mediaStream: MediaStream;
-  viewfinder: HTMLVideoElement;
-  canvas: HTMLCanvasElement;
-  additionalCaptureOptions?: DisplayMediaStreamConstraints;
-}
-
-export type PostprocessorProps = Pick<CameraProps, 'canvas'>;

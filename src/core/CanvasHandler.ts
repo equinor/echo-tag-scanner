f30df6c32281ba8f88bd1c58d515bdb5a86279a3
Torbjørn Høiland <thoil@equinor.com>
2022-08-12 09:51:17 +0200

    added back canvas width/height based on capture area

--- src/core/CanvasHandler.ts
+++ src/core/CanvasHandler.ts
@@ -73,8 +73,8 @@ class CanvasHandler {
     this.clearCanvas();
 
     // Before drawing, set the canvas dimensions to be equal to whatever is being drawn.
-    // this._canvas.width = params.dWidth ?? 0;
-    // this._canvas.height = params.dHeight ?? 0;
+    this._canvas.width = params.dWidth ?? 0;
+    this._canvas.height = params.dHeight ?? 0;
 
     if (image instanceof ImageData) {
       this._canvasContext?.putImageData(image, params.dx, params.dy);

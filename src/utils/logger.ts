e75e8408a258ed0377502421060ed4717088e09f
Erlend Andreas Hall <ehall@equinor.com>
2022-08-22 09:38:36 +0200

    console.info

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -28,77 +28,97 @@ type ScannerActionsProperties = {
 };
 
 enum LogLevel {
-  Track = 1,
-  Error,
-  Warning,
-  Info,
-  Verbose
+  Invalid = 0,
+  LocalDevelopment,
+  EchoDevelopment,
+  QA,
+  Prod
 }
-
 type LogLevelKeys = keyof typeof LogLevel;
 
 interface BaseLoggerProps {
   analytics: AnalyticsModule;
   /**
    * LogLevelOverride can be set in localstorage as "[moduleShortName].logOverride"
-   * and should be a number between 1 and 5 where highest is verbose.
+   * and should be a number between 0 and 5.
    */
   logLevelOverride?: LogLevel;
 }
 class BaseLogger {
+  protected _logLevel: LogLevel;
   protected _logLevelOverride?: LogLevel;
   protected _analytics: AnalyticsModule;
 
   constructor({ analytics, logLevelOverride }: BaseLoggerProps) {
     this._analytics = analytics;
     this._logLevelOverride = logLevelOverride;
+    this._logLevel = this.setInitialLogLevel();
+  }
+
+  public set overrideLogLevel(override: LogLevelKeys) {
+    globalThis.localStorage.setItem(
+      `${echomodule.manifest.shortName}.logOverride`,
+      String(override)
+    );
+  }
+
+  public getLogLevelOverride() {
+    const overriddenLevel = Number(
+      globalThis.localStorage.getItem(
+        `${echomodule.manifest.shortName}.logOverride`
+      )
+    );
   }
 
   public track(event: AnalyticsEvent): void {
-    if (this.logLevel() <= LogLevel.Track) return;
+    if (this._logLevel !== LogLevel.Prod) return;
     this._analytics.trackEvent(event);
   }
 
   public trackError(error: Error): void {
-    if (this.logLevel() <= LogLevel.Track) return;
+    if (this._logLevel !== LogLevel.Prod) return;
     this._analytics.logError(error);
   }
 
   public log(level: LogLevelKeys, callback: Function): void {
-    if (this.logLevel() < LogLevel[level]) return;
+    if (this._logLevel > LogLevel[level]) return;
     callback();
   }
 
   /**
    * @returns LogLevel for current hosting environment.
    */
-  protected logLevel(): LogLevel {
+  protected setInitialLogLevel(): LogLevel {
     const location = globalThis.location;
     const modulePath = echomodule.manifest.path;
 
     // We only log things when module path matches our module
-    if (!location.pathname.includes(modulePath)) return 0;
+    if (!location.pathname.includes(modulePath)) return LogLevel.Invalid;
 
-    if (this._logLevelOverride) {
-      return this._logLevelOverride;
-    }
+    // Production
+    if (location.host === 'echo.equinor.com') return LogLevel.Prod;
 
+    // Quality assurance
     if (
-      location.host === 'echo.equinor.com' ||
-      location.host === 'dt-echopedia-web-dev.azurewebsites.net' ||
       location.host === 'dt-echopedia-web-qa.azurewebsites.net' ||
       location.host === 'dt-echopedia-web-test.azurewebsites.net'
+    )
+      return LogLevel.QA;
+
+    // Remote development
+    if (location.host === 'dt-echopedia-web-dev.azurewebsites.net') {
+      return LogLevel.EchoDevelopment;
+    }
+
+    // Local development
+    if (
+      location.host.includes('localhost') ||
+      location.host.includes('loca.lt')
     ) {
-      // FIXME: We could in theory also console.log errors here. Not sure.
-      // return LogLevel.Error
-      return LogLevel.Track;
+      return LogLevel.LocalDevelopment;
     }
 
-    if (location.host === 'localhost:3000') {
-      return LogLevel.Verbose;
-    }
-
-    return LogLevel.Error;
+    return LogLevel.Invalid;
   }
 }
 

a02c1be745d129b3d6c7d5f15208035af9b91f1e
Erlend Andreas Hall <ehall@equinor.com>
2022-11-11 12:29:34 +0100

    constrained resolution to 720p

--- src/const/camera.ts
+++ src/const/camera.ts
@@ -4,8 +4,8 @@ export const zoomSteps = [1, 2, 3];
 Reflect.preventExtensions(zoomSteps);
 
 export const cameraRequest: CameraResolutionRequest = {
-  width: { min: 1280, max: 1920 },
-  height: { min: 720, max: 1080 },
+  width: { min: 1280, max: 1280 },
+  height: { min: 720, max: 720 },
 
   /** The FPS can be considered ideal, but will drop lower if the device is not capable.
    * Defaults to 60.

292097f072fb78431ff21e272106cad37dcdcc7f
Erlend Andreas Hall <ehall@equinor.com>
2022-11-17 10:11:19 +0100

    adjusted z-index for overlays as they were mistakenly stacked behind gesturearea

--- src/const/zIndex.ts
+++ src/const/zIndex.ts
@@ -1,12 +1,12 @@
 const belowViewfinder = -1;
 const viewfinder = 1;
 const scanningArea = 2;
-const overlays = 3;
 const canvas = 4;
 const cameraControls = 5;
 const gestures = 6;
 const debug = 7;
 const versionNumber = debug;
+const overlays = 8;
 const errorBoundary = 501;
 const echoBottombar = 1425;
 

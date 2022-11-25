15b7a3e798fb8d5635800c74ba7cccfc50baa6d3
Erlend Andreas Hall <ehall@equinor.com>
2022-11-25 15:20:11 +0100

    cleanup

--- src/cameraLogic/scanner.ts
+++ src/cameraLogic/scanner.ts
@@ -28,7 +28,6 @@ export class TagScanner extends Camera {
         this.videoTrack?.dispatchEvent(new Event('ended'));
       };
       globalThis.simZoom = (newZoom: ZoomSteps) => this.alterZoom(newZoom);
-      globalThis.testFiltering = this._OCR.testFiltration();
     }
   }
 

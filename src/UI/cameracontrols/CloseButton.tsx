b76b18a60ac39119879afaf23af8ed5ad9c1fddf
Erlend Andreas Hall <ehall@equinor.com>
2023-01-10 08:21:28 +0100

    The close button will now navigate to location.origin if the goBack cache is empty

--- src/UI/cameracontrols/CloseButton.tsx
+++ src/UI/cameracontrols/CloseButton.tsx
@@ -4,7 +4,11 @@ import { Icon, Button } from '@equinor/eds-core-react';
 
 export const CloseButton = (): JSX.Element => {
   function goToEcho() {
-    globalThis.history.back();
+    if (globalThis.history.state != null) history.back();
+    else {
+      // In this case, no prior navigation has happened, ie the user has directly entered the tag scanner.
+      globalThis.location.href = globalThis.location.origin;
+    }
   }
 
   return (

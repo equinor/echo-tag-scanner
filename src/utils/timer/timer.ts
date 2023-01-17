69f9a60f5f06cc8d79e491fc9c173c8c57d09831
Erlend Andreas Hall <ehall@equinor.com>
2023-01-17 13:37:52 +0100

    a timer instance is now correctly stopped

--- src/utils/timer/timer.ts
+++ src/utils/timer/timer.ts
@@ -2,6 +2,7 @@ interface TimerProps {
   maxTime?: number;
 }
 
+
 export class Timer {
   /** Time elapsed is stored as milliseconds. */
   private _timeElapsed = 0;
@@ -10,7 +11,8 @@ export class Timer {
   /** We use a max time for the timer to avoid it counting "forever".
    * This value is configurable during construction.
    */
-  private _maxTime = 512000;
+  private _maxTime = 2000;
+  private _timerId?: NodeJS.Timeout;
 
   constructor(props?: TimerProps) {
     if (typeof props?.maxTime === 'number') this._maxTime = props.maxTime;
@@ -21,21 +23,32 @@ export class Timer {
   }
 
   public start(): void {
-    this._startTime = new Date();
+    if (!this._timerId) {
+      this._startTime = new Date();
 
-    setTimeout(() => {
-      this._timeElapsed = this.stop();
-      console.warn('A timer has exceeded its max time at ', this._timeElapsed);
-    }, this._maxTime);
+      this._timerId = globalThis.setTimeout(() => {
+        this._timeElapsed = this.stop();
+        console.warn(
+          'A timer has exceeded its max time at ',
+          this._timeElapsed
+        );
+      }, this._maxTime);
+    } else console.info('A timer is already active.');
   }
 
   /** Stops the timer.
    * @returns {number} The time elapsed without decimal points.
    */
   public stop(): number {
+    if (this._timerId) {
+      clearTimeout(this._timerId);
+      this._timerId = undefined;
+    }
     if (this._startTime) {
       this._timeElapsed = Date.now() - this._startTime.getTime();
     }
     return Math.floor(this._timeElapsed);
   }
 }
+
+globalThis.timer = new Timer();

ababb93c6b340e7bd878b8f9eedf622e1b2fe5fc
Erlend Andreas Hall <ehall@equinor.com>
2023-01-23 08:26:46 +0100

    echo-search closestTagAsync can now be tested from the console

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import { EchoModuleApi } from '@equinor/echo-core';
+import { Search } from '@equinor/echo-search';
 import { App } from './App';
+
+/**
+ * Accepts a possible tag number as string value and runs it through Echo Search for validation.
+ */
+async function findClosestTag(testers: string) {
+  Search.Tags.closestTagAsync(testers)
+    .then((res) => {
+      if (res.isNotFound) console.warn('Could not find match for ' + testers);
+      else console.info(`${testers} corrected to ${res.value}`);
+    })
+    .catch((reason) => console.error(reason));
+}
+
+globalThis.echoSearch = findClosestTag;
 
 export function setup(api: EchoModuleApi): void {
   api.registerApp(App, { homeScreen: true, exactPath: false });

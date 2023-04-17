8c9cb78e008edc97e91c6794886502e3d3524947
Erlend Hall <ehall@equinor.com>
2023-04-17 09:56:50 +0200

    facingMode is now required to be environment (rear camera)

--- src/utils/camera.ts
+++ src/utils/camera.ts
@@ -90,7 +90,7 @@ function getCameraPreferences(): MediaStreamConstraints {
         ideal: cameraSettingsRequest.fps?.ideal,
       },
 
-      facingMode: { ideal: "environment" },
+      facingMode: { exact: "environment" },
     },
     audio: false,
   } as MediaStreamConstraints;

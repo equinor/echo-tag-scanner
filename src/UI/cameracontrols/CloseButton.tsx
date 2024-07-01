6daea6b471e5a0dac7a31a9f50bbb012ab916d92
Erlend Andreas Hall <ehall@equinor.com>
2024-07-01 13:51:15 +0200

    ops #165473: Close button does not respond on Safari (#110)
    * improved close procedure
    
    * simplifgied close behavior

--- src/UI/cameracontrols/CloseButton.tsx
+++ src/UI/cameracontrols/CloseButton.tsx
@@ -3,12 +3,9 @@ import styled from 'styled-components';
 import { Icon, Button } from '@equinor/eds-core-react';
 
 export const CloseButton = (): JSX.Element => {
+  
   function goToEcho() {
-    if (globalThis.history.state != null) history.back();
-    else {
-      // In this case, no prior navigation has happened, ie the user has directly entered the tag scanner.
-      globalThis.location.href = globalThis.location.origin;
-    }
+    globalThis.location.replace("/");
   }
 
   return (

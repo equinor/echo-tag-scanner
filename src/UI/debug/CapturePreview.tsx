09ffc7686d55ec8ca4542a713eb3c2a3ca3928ce
Csaba Kiss <ckiss@equinor.com>
2026-03-23 13:51:47 +0100

    removing unused imports

--- src/UI/debug/CapturePreview.tsx
+++ src/UI/debug/CapturePreview.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent, useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { Button, Icon } from '@equinor/eds-core-react';
